13877b745f48da34325bd8e93fc14fb6cd13b1a7
Its-Delimas <spencerdelimas@gmail.com>
2026-06-12 01:01:30 +0300

    fix: zod email validation

---
+++ backend/src/validators/auth.validator.ts
@@ -0,0 +1,14 @@
+import {z} from "zod"
+
+export const registerSchema = z.object({
+    emaiL: z.email('Invalid email address'),
+    password: z.string().min(6,'Password must e atleast 6 characters'),
+})
+
+export const loginSchema = z.object ({
+    email:z.email('Invalid email address'),
+    password: z.string().min(1,'Password is rewuired'),
+})
+
+export type RegisterInput = z.infer<typeof registerSchema>
+export type LoginInput = z.infer<typeof loginSchema>
